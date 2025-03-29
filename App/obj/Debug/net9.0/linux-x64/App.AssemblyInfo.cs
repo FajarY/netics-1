a35c36648fb4975418bb6cfde41bb8404a9331a0
FajarY <fajaryasodana.dev@gmail.com>
2025-03-29 22:07:59 +0700

    feat: Added deploy to Remote PC

--- App/obj/Debug/net9.0/linux-x64/App.AssemblyInfo.cs
+++ App/obj/Debug/net9.0/linux-x64/App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48f3d8b60ff2fde40b2db3d4645a47c53587edd6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35c9aa43649bbb1b28c66cd7c3cd87af69111170")]
 [assembly: System.Reflection.AssemblyProductAttribute("App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- App/run.sh
+++ App/run.sh
@@ -1,1 +1,2 @@
+docker pull fajary/netics-1:latest
 docker container run -p 8080:8080 fajary/netics-1:latest
