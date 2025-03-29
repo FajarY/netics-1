2e41a72518c4ebb5bbd4a1652e0c0cc04439ad10
FajarY <fajaryasodana.dev@gmail.com>
2025-03-29 22:19:43 +0700

    feat: Added ignore strict

--- App/obj/Debug/net9.0/linux-x64/App.AssemblyInfo.cs
+++ App/obj/Debug/net9.0/linux-x64/App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35c9aa43649bbb1b28c66cd7c3cd87af69111170")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbe2f0ce8c57dc1922adf9b346580b05b8bf9bcb")]
 [assembly: System.Reflection.AssemblyProductAttribute("App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
