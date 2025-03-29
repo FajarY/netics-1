690ce19be01e3ef2dd6a5553018702b0a82f947c
FajarY <fajaryasodana.dev@gmail.com>
2025-03-29 22:49:53 +0700

    feat: Implemented caching

--- App/compile.sh
+++ App/compile.sh
@@ -1,4 +1,4 @@
 rm -rf ../Dockerfile/publish
 mkdir ../Dockerfile/publish
-dotnet publish -c Release -f net9.0 -r linux-x64 --property:PublishDir=../Dockerfile/publish
+dotnet publish -c Release -f net9.0 -r linux-x64 --no-restore --property:PublishDir=../Dockerfile/publish
 cp .env ../Dockerfile/publish
