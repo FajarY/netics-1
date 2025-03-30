1bec1179767acedf7592d85de240c88a845d1df3
FajarY <fajaryasodana.dev@gmail.com>
2025-03-30 12:05:11 +0700

    feat: Finished everything

--- App/run.sh
+++ App/run.sh
@@ -1,2 +1,2 @@
 sudo docker pull fajary/netics-1:latest
-sudo docker container run -p 8080:8080 fajary/netics-1:latest
+sudo docker container run -d -p 8080:8080 --name netics-1 fajary/netics-1:latest
