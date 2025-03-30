1eb4a7ea555fa18fd7189ca82919567891e51123
FajarY <fajaryasodana.dev@gmail.com>
2025-03-30 12:14:35 +0700

    feat: Added auto replace

--- App/run.sh
+++ App/run.sh
@@ -1,2 +1,4 @@
 sudo docker pull fajary/netics-1:latest
+sudo docker container stop netics-1 2>/dev/null || true
+sudo docker container rm netics-1 2>/dev/null || true
 sudo docker container run -d -p 8080:8080 --name netics-1 fajary/netics-1:latest
